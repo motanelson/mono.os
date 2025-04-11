e60dc2d4a12729a24806fe10ab2b39e515657647
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-04-11 16:01:45 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,29 +2,46 @@
 using System.Runtime.Serialization.Formatters.Binary;
 using System.ComponentModel;
 using System.Diagnostics;
-bool t=true;
-int n=0;
-int nn=0;
-String s="";
-Console.BackgroundColor=ConsoleColor.DarkYellow;
-Console.ForegroundColor = ConsoleColor.Black;
-Console.Clear();
-Console.WriteLine("mono os ");
-while(t){
-    s=Console.ReadLine();
-    try{
-        ProcessStartInfo psi = new ProcessStartInfo();
-	psi.FileName = "cmd.exe" ;
-	psi.UseShellExecute = false;
-	psi.Arguments ="/C "+s+ "" ;
-	psi.RedirectStandardOutput = false;
-	Process p = Process.Start(psi);
-	p = Process.Start(psi);
-	Console.WriteLine(p.StandardOutput.ReadToEnd());
-	p.WaitForExit();
-	p.Close();
-    }catch(Exception ee ){
-	Console.WriteLine("ERROR ");
-	}
+using System;
+namespace cmds{
+	
+	class monoos{
+		public class Program{
+                    static void Main(string[] args){
+                        bool t=true;
+                        int n=0;
+                        int nn=0;
+                        String s="";
+                        Console.BackgroundColor=ConsoleColor.DarkYellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        Console.WriteLine("mono os ");
+                        while(t){
+                            Console.Write(":>");
+                            s=Console.ReadLine();
+                            if (s.IndexOf("exit")>-1){
+                                t=false;
+                            }else{
+                                try{
+                                    ProcessStartInfo psi = new ProcessStartInfo();
+	                                psi.FileName = "cmd.exe" ;
+	                                psi.UseShellExecute = false;
+	                                psi.Arguments ="/C "+s+ "" ;
+	                                psi.RedirectStandardOutput = false;
+	                                Process p = Process.Start(psi);
+	                                p = Process.Start(psi);
+	                                Console.WriteLine(p.StandardOutput.ReadToEnd());
+	                                p.WaitForExit();
+	                                p.Close();
+                                }catch(Exception ee ){
+	                                 Console.WriteLine("ERROR ");
+	                            }
+                            }
+                        }
+                    }
+                
+                }
+            }
+        }
     
-}
+    
